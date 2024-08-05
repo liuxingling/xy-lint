d07e9994c980dc95b53ceacdf6d1688cc40a35f6
刘 星玲 <286735685@qq.com>
2024-08-05 13:35:51 +0800

    fix: 测试vuepress没有样式的问题-2

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,21 +1,11 @@
 import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
+import { defineUserConfig } from 'vuepress'
 
-module.exports = {
+export default defineUserConfig({
   bundler: viteBundler(),  // 使用 Vite 作为打包工具
-  viteOptions: {
-    base: '/xy-lint/'
-  },
   theme: defaultTheme(),   // 使用默认主题
+  base: '/xy-lint/',
   title: '编码规范文档',
   description: '为项目提供编码规范的文档说明',
-  themeConfig: {
-    sidebar: [
-      '/',
-      '/eslint/',
-      '/stylelint/',
-      '/commitlint/',
-      '/markdownlint/'
-    ]
-  }
-}
+})
